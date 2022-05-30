9506c6c4666254377c9eef40e147575c03402011
Salimatou-S <mascindye2016@gmail.com>
2022-05-30 12:37:59 +0200

    mise en forme des pages

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -16,6 +16,22 @@ class PageController extends AbstractController
         ]);
     }
 
+    #[Route('/femmewax', name: 'femme_wax')]
+    public function femmewax(): Response
+    {
+        return $this->render('page/femmewax.html.twig', [
+            'controller_name' => 'FemmewaxController',
+        ]);
+    }
+
+    #[Route('/femmebazin', name: 'femme_bazin')]
+    public function femmebazin(): Response
+    {
+        return $this->render('page/femmebazin.html.twig', [
+            'controller_name' => 'FemmebazinController',
+        ]);
+    }
+
     #[Route('/fille', name: 'page_fille')]
     public function fille(): Response
     {
@@ -24,11 +40,43 @@ class PageController extends AbstractController
         ]);
     }
 
+    #[Route('/fillebazin', name: 'fille_bazin')]
+    public function fillebazin(): Response
+    {
+        return $this->render('page/fillebazin.html.twig', [
+            'controller_name' => 'FillebazinController',
+        ]);
+    }
+
+    #[Route('/fillewax', name: 'fille_wax')]
+    public function fillewax(): Response
+    {
+        return $this->render('page/fillewax.html.twig', [
+            'controller_name' => 'FillewaxController',
+        ]);
+    }
+
     #[Route('/accessoires', name: 'page_accessoires')]
     public function accessoires(): Response
     {
         return $this->render('page/accessoires.html.twig', [
             'controller_name' => 'AccessoiresController',
+        ]);
+    }
+
+    #[Route('/accessoiresSC', name: 'accessoires_SC')]
+    public function accessoiresSC(): Response
+    {
+        return $this->render('page/accessoiresSC.html.twig', [
+            'controller_name' => 'AccessoiresSCController',
+        ]);
+    }
+
+    #[Route('/accessoiresBJ', name: 'accessoires_BJ')]
+    public function accessoiresBJ(): Response
+    {
+        return $this->render('page/accessoiresBJ.html.twig', [
+            'controller_name' => 'AccessoiresBJController',
         ]);
     }
 }
