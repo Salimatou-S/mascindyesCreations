554a31aaa4a653e27201cfa9915685098b03203f
Salimatou-S <mascindye2016@gmail.com>
2022-06-01 14:55:55 +0200

    modif en tite categorie

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -52,6 +52,11 @@ class Category
         $this->produits = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+       return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
